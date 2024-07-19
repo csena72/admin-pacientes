61bc52fdd21a6967565b2005639ccbf6f9035c06
Cristian Sena <crisianjsena@gmail.com>
2024-07-19 19:15:58 -0300

    Agrego formulario y validacion

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,33 @@
 <script setup>
 
+import { reactive } from 'vue'
+import Alerta from './Alerta.vue'
+
+const alerta = reactive({
+    tipo: '',
+    mensaje: '',
+})
+
+const paciente = reactive({
+    nombre: '',
+    propietario: '',
+    email: '',
+    alta: '',
+    sintomas: '',
+})
+
+const validar = () => {
+    if(Object.values(paciente).includes('')) {
+        alerta.mensaje = 'Todos los campos son obligatorios'
+        alerta.tipo = 'error'
+        return alerta
+    }
+
+    alerta.mensaje = 'Paciente registrado con exito'
+    alerta.tipo = 'correcto'
+    return alerta
+}
+
 </script>
 
 <template>
@@ -9,7 +37,14 @@
             Añade Pacientes y <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
 
-        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <Alerta
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        />
+        <form
+            @submit.prevent="validar"
+            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        >
             <div class="mb-5">
                 <label 
                     for="mascota" 
@@ -22,6 +57,7 @@
                     id="mascota" 
                     placeholder="Nombre de la mascota" 
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.nombre"
                 />
             </div>
             <div class="mb-5">
@@ -36,6 +72,7 @@
                     id="propietario" 
                     placeholder="Nombre del propietario" 
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.propietario"
                 />
             </div>
             <div class="mb-5">
@@ -50,6 +87,7 @@
                     id="email" 
                     placeholder="Email del propietario" 
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.email"
                 />
             </div>
             <div class="mb-5">
@@ -63,6 +101,7 @@
                     type="date" 
                     id="alta"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    v-model="paciente.alta"
                 />
             </div>
             <div class="mb-5">
@@ -76,11 +115,12 @@
                     id="sintomas"
                     placeholder="Describe los síntomas"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                    v-model="paciente.sintomas"
                 />
             </div>
 
-            <input 
-                type="submit" 
+            <input
+                type="submit"
                 class="bg-indigo-600 w-full p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-700 cursor-pointer transition-colors" 
                 value="Registrar Paciente"
             />
